bacfa1f938ababc28424506923d7fc83f9e34450
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2025-07-11 17:23:22 +0100

    Add rotation animation to UI elements

--- Assets/UIAnimations/RotateUI.cs
+++ Assets/UIAnimations/RotateUI.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using DG.Tweening;
 
+/// <summary>
+/// Rotates a UI element using DOTween, driven by a serialized AnimationCurve.
+/// Uses RectTransform for UI rotation.
+/// </summary>
 public class RotateUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Animation Settings")]
+    [SerializeField] private AnimationCurve rotationCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    [SerializeField] private float duration = 1f;
+    [SerializeField] private Vector3 targetRotation = new Vector3(0, 0, 360);
+    [SerializeField] private bool playOnStart = true;
+
+    private RectTransform rectTransform;
+    private Vector3 initialRotation;
+    private Tween rotationTween;
+
+    void Awake()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        if (rectTransform == null)
+        {
+            Debug.LogError("RotateUI requires a RectTransform component.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        initialRotation = rectTransform.localEulerAngles;
+        if (playOnStart)
+        {
+            PlayRotation();
+        }
+    }
+
+    /// <summary>
+    /// Plays the rotation animation using the AnimationCurve.
+    /// </summary>
+    public void PlayRotation()
+    {
+        rotationTween?.Kill();
+
+        float t = 0f;
+        rotationTween = DOTween.To(() => t, x => {
+            t = x;
+            float curveValue = rotationCurve.Evaluate(t);
+            rectTransform.localEulerAngles = initialRotation + targetRotation * curveValue;
+        }, 1f, duration)
+        .SetEase(Ease.Linear)
+        .OnComplete(() => t = 1f);
+    }
+
+    /// <summary>
+    /// Optionally, call this to reset and replay the animation.
+    /// </summary>
+    public void ResetAndPlay()
+    {
+        rectTransform.localEulerAngles = initialRotation;
+        PlayRotation();
     }
 }
